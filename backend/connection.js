e7c4260233044f74cdf43390d86ebf91a94f18e5
AxelCHCR <axelchaves.r@estudiantec.cr>
2023-12-28 16:56:13 -0600

    feat: api created

---
+++ backend/connection.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const portDefault = 4000;
+const app = express();
+const port = process.env.PORT || portDefault;
+app.get("/", (req, res) => {
+    res.send("API for Duende Maquillista");
+  });
+//mongoose.connect("");
+app.listen(port, () => console.log(`Server running on port ${port}`));
