fc626291cac3df45748775a53e2b4d46d1cb2e27
AxelCHCR <axelchaves.r@estudiantec.cr>
2023-12-28 17:25:11 -0600

    feat: connection.js API created and user model created

---
+++ backend/models/userSchema.js
@@ -0,0 +1,23 @@
+mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    lastName:{
+        type: String, 
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String, 
+        required: true,
+    }
+});
