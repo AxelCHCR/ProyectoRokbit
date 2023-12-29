30d39047c73135ec2a9264de012c276aa13a8ecc
Javier Vasquez <javiervasquez212@gmail.com>
2023-12-29 12:32:38 -0600

    feat: added HTMLInputElement attributes to input component

--- src/app/components/inputs.tsx
+++ src/app/components/inputs.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 
-interface InputProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
   className?: string;
 }
 
-const Input: React.FC<InputProps> = ({ placeholder, className }) => {
-  const inputClass = `border border-dark-gray focus:outline-none rounded-lg bg-custom-gray w-40 h-12 font-poppins font-light px-4 ${
-    className || ""
-  }`;
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ placeholder, className, ...rest }, ref) => {
+    const inputClass = `border border-dark-gray focus:outline-none rounded-lg bg-custom-gray w-40 h-12 font-poppins font-light px-4 ${
+      className || ""
+    }`;
 
-  return <input className={inputClass} placeholder={placeholder} />;
-};
+    return (
+      <input
+        ref={ref}
+        className={inputClass}
+        placeholder={placeholder}
+        {...rest}
+      />
+    );
+  }
+);
 
 export default Input;
