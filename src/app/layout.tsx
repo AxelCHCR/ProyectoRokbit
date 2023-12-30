6a41ad32af3cd1f3dad2d880705de67af04c5f4c
Kev1606 <kevincruz1606@estudiantec.cr>
2023-12-30 17:37:25 -0600

    Feat: new page for noAuthRequired added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,17 @@
 "use client";
-// import type { Metadata } from "next";
 import "./globals.css";
 import { poppins } from "@/utils/fonts";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ProtectedRoute } from "./components/protected";
 import { usePathname } from "next/navigation";
 
-// export const metadata: Metadata = {
-//   title: "Colibri",
-// };
-
-const noAuthRequired = ["/", "/pages/register"];
+const noAuthRequired = ["/", "/pages/register", "/pages/recoverPassword"];
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const router = useRouter();
   const isAuthRequired = !noAuthRequired.includes(usePathname());
   return (
     <html lang="en">
