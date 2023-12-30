433f3c2f31ff73a83c6fa04d50b43e3174aac8bf
Kev1606 <kevincruz1606@estudiantec.cr>
2023-12-30 15:54:30 -0600

    Fix: Refactor layout component to include protected routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,33 @@
-import type { Metadata } from "next";
+"use client";
+// import type { Metadata } from "next";
 import "./globals.css";
 import { poppins } from "@/utils/fonts";
 import { AuthContextProvider } from "./context/AuthContext";
+import { ProtectedRoute } from "./components/protected";
+import { usePathname } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "Colibri",
-};
+// export const metadata: Metadata = {
+//   title: "Colibri",
+// };
+
+const noAuthRequired = ["/", "/pages/register"];
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // const router = useRouter();
+  const isAuthRequired = !noAuthRequired.includes(usePathname());
   return (
     <html lang="en">
       <body className={`${poppins}`}>
         <AuthContextProvider>
-          {children}
+          {isAuthRequired ? (
+            <ProtectedRoute>{children}</ProtectedRoute>
+          ) : (
+            children
+          )}
         </AuthContextProvider>
       </body>
     </html>
