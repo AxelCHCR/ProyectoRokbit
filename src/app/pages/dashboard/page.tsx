75ff79f5d2eb59c2598d89e1653f2044fe98858e
Kev1606 <kevincruz1606@estudiantec.cr>
2024-01-03 10:39:16 -0600

    Feat: manage roles

--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -1,12 +1,22 @@
 "use client"
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from "@/app/context/AuthContext";
 import { useRouter } from "next/navigation";
+import UserController from '../../../../backend/controllers/UserController';
 
 export default function Home() {
   const { user, logout } = useAuth();
+  const [ role, setRole ]  = useState("");
   const router = useRouter();
-  console.log(user);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const response = await UserController.get("http://localhost:4000/api/getUser", { params: { email: user.email } });
+      setRole(response.role);
+    };
+    fetchData();
+  }, [role]);
+
   const handleSignOut = async () => {
     try {
       await logout();
@@ -30,6 +40,11 @@ export default function Home() {
       <button onClick={profile}>See Profile</button>
       <button onClick={() => router.push("/pages/disponibility")}>Disponibility</button>
       <button onClick={() => router.push("/pages/notifications")}>Notifications</button>
+      {role === 'User' && ( // Validación del rol de Admin para mostrar el botón
+        <button onClick={() => {
+          alert("User button clicked");
+        }}>User Button</button>
+      )}
     </div></>
   );
 }
